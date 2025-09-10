01226c29ab73fc2e9d60d19b6c9664775b5a6635
Min Soo Kang <minsookang33@outlook.com>
2025-09-10 18:04:01 +1000

    add info

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,19 @@
-import LoginButton from "../auth/login";
+import { Info } from "lucide-react";
 
 export default function Header() {
   return (
     <>
-    SpQL Workbench
+    <div className="flex">SpQL Workbench <a
+        href="https://github.com/minnysoop/siteshack2025"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="More info"
+        style={{ display: "flex", alignItems: "center" }}
+        className="ml-2 text-green-500"
+      >
+        <Info size={20} strokeWidth={2} />
+      </a></div>
+   
     </>
   );
 }
